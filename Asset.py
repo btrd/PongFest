c8ab301d88b1884124c211b9a07b11ac2d34b683
Jean Bertrand <janus24@japple.local>
2014-06-29 14:58:23 +0200

    Beginning

---
+++ Asset.py
@@ -0,0 +1,39 @@
+import EdgeLaser
+
+v=20
+
+class Asset:
+
+	def __init__(self, game, x1, y1, x2, y2):
+		self.game = game
+
+		#coin sup gauche
+		self.x1 = x1
+		self.y1 = y1
+
+		#coin inf droite
+		self.x2 = x2
+		self.y2 = y2
+
+class Player(Asset):
+	def __init__(self, game, num):
+		if num==1:
+			x=5;
+		else:
+			x=495
+		Asset.__init__(self, game, x, 250, x, 350)
+		self.sco = 0
+		self.num = num
+
+	def draw(self):
+			self.game.addLine(self.x1, self.y1, self.x2, self.y2, EdgeLaser.LaserColor.LIME)
+
+	def up(self):
+		if self.y1>100:
+			self.y1 += v
+			self.y2 += v
+
+	def down(self):
+		if self.y2<500:
+			self.y1 -= v
+			self.y2 -= v
---
+++ PongFest.py
@@ -0,0 +1,35 @@
+import EdgeLaser
+import time
+import Asset
+
+game = EdgeLaser.LaserGame('PongFest')
+
+game.setResolution(500).setDefaultColor(EdgeLaser.LaserColor.LIME)
+
+p1 = Asset.Player(game,1)
+p2 = Asset.Player(game,2)
+
+while game.isStopped():
+	game.receiveServerCommands()
+
+while not game.isStopped():
+	
+	game.receiveServerCommands()
+
+	if game.player1_keys:
+		if game.player1_keys.yn :
+			p1.up()
+		if game.player1_keys.yp :
+			p1.down()
+
+	if game.player2_keys:
+		if game.player2_keys.yn :
+			p2.up()
+		if game.player2_keys.yp :
+			p2.down()
+
+	game.addLine(0, 100, 500, 100, EdgeLaser.LaserColor.LIME)
+	p1.draw()
+	p2.draw()
+	game.refresh()
+	time.sleep(0.05)
