dc0a009e9198a4dc1737026f4542afecfd4af4b8
Jean Bertrand <jeanus24@gmail.com>
2014-06-30 21:05:02 +0200

    gestion partiel de la balle

--- Asset.py
+++ Asset.py
@@ -1,6 +1,8 @@
 import EdgeLaser
+import random
 
-v=20
+ps=20
+bs=10
 
 class Asset:
 
@@ -22,7 +24,7 @@ class Player(Asset):
 		else:
 			x=495
 		Asset.__init__(self, game, x, 250, x, 350)
-		self.sco = 0
+		self.score = 0
 		self.num = num
 		self.lose = False
 
@@ -30,17 +32,64 @@ class Player(Asset):
 			self.game.addLine(self.x1, self.y1, self.x2, self.y2, EdgeLaser.LaserColor.LIME)
 
 	def up(self):
+		#On test si on va pas sortir du terrain de jeux
 		if self.y1>120:
-			self.y1 -= v
-			self.y2 -= v
+			self.y1 -= ps
+			self.y2 -= ps
 		else:
-			self.y1 = 100
-			self.y2 = 200
+			self.y1 = 101
+			self.y2 = 201
 
 	def down(self):
+		#On test si on va pas sortir du terrain de jeux
 		if self.y2<480:
-			self.y1 += v
-			self.y2 += v
+			self.y1 += ps
+			self.y2 += ps
 		else:
-			self.y1 = 400
-			self.y2 = 500
+			self.y1 = 399
+			self.y2 = 499
+
+class Ball(Asset):
+	def __init__(self, game):
+		Asset.__init__(self, game, 240, 290, 260, 310)
+		self.v=5
+		#direction de depart aleatoire
+		if random.randint(0,2) == 1:
+			self.dx=-bs
+		else:
+			self.dx=bs
+		self.dy=0
+
+	def __str__(self):
+		return "x1:{self.x1};x2:{self.x2};y1:{self.y1};y2:{self.y2}"
+
+	def draw(self):
+		#test si on sort pas du cadre
+		self.x2+=self.dx
+		self.y2+=self.dy
+		self.y1+=self.dy
+		self.x1+=self.dx
+		self.game.addRectangle(self.x1, self.y1, self.x2, self.y2, EdgeLaser.LaserColor.LIME)
+
+	def conflict(self, p1, p2):
+		print self.x2
+		if self.x1 <= 5:
+			print ':1'
+			#test si p1 est sur la balle
+			if (self.y2>p1.y1 and self.y2<p1.y2) or (self.y1>p1.y1 and self.y1<p1.y2):
+				#p1 rattrape la balle
+			else:
+				#p1 perdu
+				p2.score+=1
+		elif self.x2>=495:
+			#test si p2 est sur la balle
+			if (self.y2>p2.y1 and self.y2<p2.y2) or (self.y1>p2.y1 and self.y1<p2.y2):
+				#p2 rattrape la balle
+
+			else:
+				#p2 perdu
+				p1.score+=1
+		elif self.y1<=101 or self.y1>=499:
+			#touch les bords
+		else:
+			self.draw()
--- PongFest.py
+++ PongFest.py
@@ -3,34 +3,47 @@ import time
 import Asset
 
 game = EdgeLaser.LaserGame('PongFest')
+font = EdgeLaser.LaserFont('lcd.elfc')
 
 game.setResolution(500).setDefaultColor(EdgeLaser.LaserColor.LIME)
 
 p1 = Asset.Player(game,1)
 p2 = Asset.Player(game,2)
 
+goal = True
+
 while game.isStopped():
 	game.receiveServerCommands()
 
 while not game.isStopped():
 	game.receiveServerCommands()
+	if goal:
+		b = Asset.Ball(game)
+		goal = False
 
 	#afficher le score
+	font.render(game, ("%d"%p1.score), 0, 10, EdgeLaser.LaserColor.LIME, 7)
+	font.render(game, ("%d"%p2.score), 430, 10, EdgeLaser.LaserColor.LIME, 7)
 
+	#mouvements des joueurs
 	if game.player1_keys:
 		if game.player1_keys.yp :
 			p1.up()
 		if game.player1_keys.yn :
 			p1.down()
-
 	if game.player2_keys:
 		if game.player2_keys.yp :
 			p2.up()
 		if game.player2_keys.yn :
 			p2.down()
 
+	#ligne de separation
 	game.addLine(0, 100, 500, 100, EdgeLaser.LaserColor.LIME)
+	game.addLine(0, 500, 500, 500, EdgeLaser.LaserColor.LIME)
+	#affichage des joueurs
 	p1.draw()
 	p2.draw()
+	b.conflict(p1,p2)
+
 	game.refresh()
 	time.sleep(0.05)
