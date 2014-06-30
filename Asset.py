8bca162a9004f7cf51ad3d4fff3bd2e538907621
Jean Bertrand <jeanus24@gmail.com>
2014-07-01 00:15:08 +0200

    Jeux presque fonctionnel

--- Asset.py
+++ Asset.py
@@ -1,8 +1,9 @@
 import EdgeLaser
-import random
 
-ps=20
-bs=10
+#global player speed
+Gps=20
+#global ball speed
+Gbs=10
 
 class Asset:
 
@@ -26,16 +27,16 @@ class Player(Asset):
 		Asset.__init__(self, game, x, 250, x, 350)
 		self.score = 0
 		self.num = num
-		self.lose = False
+		self.win = False
 
 	def draw(self):
-			self.game.addLine(self.x1, self.y1, self.x2, self.y2, EdgeLaser.LaserColor.LIME)
+			self.game.addLine(self.x1, self.y1, self.x2, self.y2)
 
 	def up(self):
 		#On test si on va pas sortir du terrain de jeux
 		if self.y1>120:
-			self.y1 -= ps
-			self.y2 -= ps
+			self.y1 -= Gps
+			self.y2 -= Gps
 		else:
 			self.y1 = 101
 			self.y2 = 201
@@ -43,53 +44,72 @@ class Player(Asset):
 	def down(self):
 		#On test si on va pas sortir du terrain de jeux
 		if self.y2<480:
-			self.y1 += ps
-			self.y2 += ps
+			self.y1 += Gps
+			self.y2 += Gps
 		else:
 			self.y1 = 399
 			self.y2 = 499
 
 class Ball(Asset):
-	def __init__(self, game):
+	def __init__(self, game,p):
+		print p
+		self.bs=Gbs
 		Asset.__init__(self, game, 240, 290, 260, 310)
 		self.v=5
 		#direction de depart aleatoire
-		if random.randint(0,2) == 1:
-			self.dx=-bs
+		if p == 1:
+			self.dx=-self.bs
 		else:
-			self.dx=bs
+			self.dx=self.bs
 		self.dy=0
 
 	def __str__(self):
 		return "x1:{self.x1};x2:{self.x2};y1:{self.y1};y2:{self.y2}"
 
 	def draw(self):
-		#test si on sort pas du cadre
 		self.x2+=self.dx
 		self.y2+=self.dy
 		self.y1+=self.dy
 		self.x1+=self.dx
-		self.game.addRectangle(self.x1, self.y1, self.x2, self.y2, EdgeLaser.LaserColor.LIME)
+		#test si on sort pas du cadre
+		if self.x1<=5:
+			self.x1 = 6
+			self.x2 = 26
+		if self.x2>=495:
+			self.x1 = 474
+			self.x2 = 494
+		if self.y1<=100:
+			self.y1 = 101
+			self.y2 = 121
+		if self.y2>=500:
+			self.y1 = 479
+			self.y2 = 499
+		self.game.addRectangle(self.x1, self.y1, self.x2, self.y2)
 
 	def conflict(self, p1, p2):
-		print self.x2
-		if self.x1 <= 5:
-			print ':1'
+		if self.x1 == 6:
 			#test si p1 est sur la balle
 			if (self.y2>p1.y1 and self.y2<p1.y2) or (self.y1>p1.y1 and self.y1<p1.y2):
 				#p1 rattrape la balle
+				self.dx=-self.dx
+				self.dy=-self.dy
 			else:
 				#p1 perdu
 				p2.score+=1
-		elif self.x2>=495:
+				p2.win=True
+		elif self.x2==494:
 			#test si p2 est sur la balle
 			if (self.y2>p2.y1 and self.y2<p2.y2) or (self.y1>p2.y1 and self.y1<p2.y2):
 				#p2 rattrape la balle
-
+				self.dx=-self.dx
+				self.dy=-self.dy
 			else:
 				#p2 perdu
 				p1.score+=1
-		elif self.y1<=101 or self.y1>=499:
-			#touch les bords
-		else:
+				p1.win=True
+		elif self.y1==101 or self.y1==499:
+			#touche les bords
+			print 'todo'
+
+		if not p1.win and not p2.win:
 			self.draw()
