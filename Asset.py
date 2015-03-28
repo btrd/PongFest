6246f624fbfcc913f95094e8566ad75196eb7ce1
Jean Bertrand <jeanus24@gmail.com>
2015-03-28 01:16:22 +0100

    Envoie du score + envoie position ball

--- Asset.py
+++ Asset.py
@@ -1,9 +1,12 @@
 import EdgeLaser
+import json
+import requests
+import copy
 
 #global player speed
-Gps=10
+Gps=5
 #global ball speed
-Gbs=5
+Gbs=3
 
 class Asset:
 
@@ -30,7 +33,7 @@ class Player(Asset):
 		self.win = False
 
 	def draw(self):
-			self.game.addLine(self.x1, self.y1, self.x2, self.y2)
+		self.game.addLine(self.x1, self.y1, self.x2, self.y2)
 
 	def up(self):
 		#On test si on va pas sortir du terrain de jeux
@@ -52,35 +55,35 @@ class Player(Asset):
 
 class Ball(Asset):
 	def __init__(self, game,p):
-		self.bs=Gbs
+		self.bs = Gbs
 		Asset.__init__(self, game, 240, 290, 260, 310)
 		self.v=5
 		#direction de depart aleatoire
 		if p == 1:
-			self.dx=-self.bs
+			self.dx = -self.bs
 		else:
-			self.dx=self.bs
+			self.dx = self.bs
 		self.dy=0
 
 	def __str__(self):
 		return "x1:{self.x1};x2:{self.x2};y1:{self.y1};y2:{self.y2}"
 
 	def draw(self):
-		self.x2+=self.dx
-		self.y2+=self.dy
-		self.y1+=self.dy
-		self.x1+=self.dx
+		self.x2 += self.dx
+		self.y2 += self.dy
+		self.y1 += self.dy
+		self.x1 += self.dx
 		#test si on sort pas du cadre
-		if self.x1<=5:
+		if self.x1 <= 5:
 			self.x1 = 6
 			self.x2 = 26
-		if self.x2>=495:
+		if self.x2 >= 495:
 			self.x1 = 474
 			self.x2 = 494
-		if self.y1<=100:
+		if self.y1 <= 100:
 			self.y1 = 101
 			self.y2 = 121
-		if self.y2>=500:
+		if self.y2 >= 500:
 			self.y1 = 479
 			self.y2 = 499
 		self.game.addRectangle(self.x1, self.y1, self.x2, self.y2)
@@ -88,36 +91,64 @@ class Ball(Asset):
 	def conflict(self, p1, p2):
 		if self.x1 == 6:
 			#test si p1 est sur la balle
-			if (self.y2>p1.y1 and self.y2<p1.y2) or (self.y1>p1.y1 and self.y1<p1.y2):
+			if (self.y2 > p1.y1 and self.y2 < p1.y2) or (self.y1 > p1.y1 and self.y1 < p1.y2):
 				#p1 rattrape la balle
 				#centre de la balle
 				yb = self.y1+10
 				#calcul trajectoire sur y en prenant la distance par rapport au centre de la raquette
 				self.dy = (yb-p1.y1-50)*0.2
 				#inversion du sens de la balle + augmentation vitesse
-				self.dx = -self.dx+1
+				self.dx = -self.dx
+				#calcul fictif du parcours de la balle
+				self.fictif_route()
 			else:
 				#p1 perdu
-				p2.score+=1
+				p2.score += 1
 				p2.win=True
-		elif self.x2==494:
+				self.sendScore(p1, p2)
+		elif self.x2 == 494:
 			#test si p2 est sur la balle
-			if (self.y2>p2.y1 and self.y2<p2.y2) or (self.y1>p2.y1 and self.y1<p2.y2):
+			if (self.y2 > p2.y1 and self.y2 < p2.y2) or (self.y1 > p2.y1 and self.y1 < p2.y2):
 				#p2 rattrape la balle
 				#centre de la balle
 				yb = self.y1+10
 				#calcul trajectoire sur y en prenant la distance par rapport au centre de la raquette
 				self.dy = (yb-p2.y1-50)*0.2
 				#inversion du sens de la balle + augmentation vitesse
-				self.dx = -self.dx-1
+				self.dx = -self.dx
+				#calcul fictif du parcours de la balle
+				self.fictif_route()
 			else:
 				#p2 perdu
-				p1.score+=1
-				p1.win=True
-		elif self.y1==101 or self.y2==499:
+				p1.score += 1
+				p1.win = True
+				self.sendScore(p1, p2)
+		elif self.y1 == 101 or self.y2 == 499:
 			#touche les bords
-			self.dy=-self.dy
+			self.dy = -self.dy
 
 		if not p1.win and not p2.win:
 			self.draw()
-			
+
+	def fictif_route(self):
+		b = copy.copy(self)
+		#tant qu'on est pas sur les bords
+		while b.x1 > 6 and b.x2 < 494:
+			#si on touche le sol ou le plafond
+			if b.y1 <= 101 or b.y2 >= 499:
+				b.dy = -b.dy
+			b.x2 += b.dx
+			b.y2 += b.dy
+			b.y1 += b.dy
+			b.x1 += b.dx
+
+		#diffusion
+		data = {'x1':b.x1,'x2':b.x2,'y1':b.x1,'y2':b.x2}
+		data_json = json.dumps(data)
+		payload = {'json_playload': data_json}
+		#r = requests.post('http://shadok-pong.scalingo.io/api/fictif', data=payload)
+	
+	def sendScore(self, p1, p2):
+		data = {'left':p1.score,'right':p2.score}
+		headers = {'Content-Type':'application/json'}
+		r = requests.post('http://shadok-pong.scalingo.io/api/score', data=json.dumps(data), headers=headers)
--- PongFest.py
+++ PongFest.py
@@ -23,52 +23,33 @@ while True:
 	while not game.isStopped():
 		game.receiveServerCommands()
 
-		if p1.score == 5:
-			#afficher le score
-			font.render(game, "P1", 200, 100, EdgeLaser.LaserColor.LIME, 10)
-			font.render(game, "WIN", 150, 300, EdgeLaser.LaserColor.LIME, 10)
+		#ligne de separation
+		game.addLine(0, 100, 500, 100)
+		game.addLine(0, 500, 500, 500)
+		if p1.win:
+			b = Asset.Ball(game,1)
+			p1.win = False
+		elif p2.win:
+			b = Asset.Ball(game,2)
+			p2.win = False
 
-			#time.sleep(3)
-			#game.pause
-		elif p2.score == 5:
-			#afficher le score
-			font.render(game, "P2", 200, 100, EdgeLaser.LaserColor.LIME, 10)
-			font.render(game, "WIN", 150, 300, EdgeLaser.LaserColor.LIME, 10)
+		#mouvements des joueurs
+		if game.player1_keys:
+			if game.player1_keys.yp :
+				p1.up()
+			if game.player1_keys.yn :
+				p1.down()
+		if game.player2_keys:
+			if game.player2_keys.yp :
+				p2.up()
+			if game.player2_keys.yn :
+				p2.down()
 
-			#time.sleep(3)
-			#game.pause
-		else:
-			#ligne de separation
-			game.addLine(0, 100, 500, 100)
-			game.addLine(0, 500, 500, 500)
-			#afficher le score
-			font.render(game, ("%d"%p1.score), 0, 10, EdgeLaser.LaserColor.LIME, 7)
-			font.render(game, ("%d"%p2.score), 430, 10, EdgeLaser.LaserColor.LIME, 7)
+		b.conflict(p1,p2)
 
-			if p1.win:
-				b = Asset.Ball(game,1)
-				p1.win = False
-			elif p2.win:
-				b = Asset.Ball(game,2)
-				p2.win = False
-
-			#mouvements des joueurs
-			if game.player1_keys:
-				if game.player1_keys.yp :
-					p1.up()
-				if game.player1_keys.yn :
-					p1.down()
-			if game.player2_keys:
-				if game.player2_keys.yp :
-					p2.up()
-				if game.player2_keys.yn :
-					p2.down()
-
-			b.conflict(p1,p2)
-
-			#affichage des joueurs
-			p1.draw()
-			p2.draw()
+		#affichage des joueurs
+		p1.draw()
+		p2.draw()
 
 		game.refresh()
 		time.sleep(0.02)
