e904d3964ac3b3deb8c1c6a4a4fc9c11ee03d8ce
Yannick Jost <yannick@yjost.com>
2015-04-09 20:55:50 +0200

    WIP

--- Asset.py
+++ Asset.py
@@ -1,5 +1,6 @@
 import EdgeLaser
 import json
+import math
 import requests
 import copy
 from sessions import FuturesSession
@@ -13,6 +14,92 @@ server="http://shadok-pong.scalingo.io"
 #server="http://localhost:3000"
 
 session = FuturesSession()
+
+def apply_rot(angle,x,y):
+    return x*math.cos(angle)-y*math.sin(angle) , x*math.sin(angle)+y*math.cos(angle)
+
+
+def apply_trans(obj,x,y):
+    return obj.x+x,obj.y+y
+
+def determinant(vec1,vec2):
+    return vec1.x*vec2.y-vec1.y*vec2.x
+
+
+def lines_intersect(a,b,c,d):
+    det=determinant(b-a,c-d)
+    if det == 0 :
+        return None
+    t=determinant(c-a,c-d)/float(det)
+    u=determinant(b-a,c-a)/float(det)
+
+    if ((t<0) or (u<0) or (t>1) or (u>1)) :
+        return None
+    return a*(1-t)+t*b
+
+class Angle(object):
+    def __init__(self, value):
+        if isinstance(value, Angle):
+            self.value = value.value
+        else :
+            self.value = value
+
+    def normalize(self):
+        if self.value < -math.pi:
+            self.value+=2*math.pi
+        elif self.value > math.pi:
+            self.value-=2*math.pi
+
+    def __add__(self, value):
+        self.value+=value
+        self.normalize()
+        return self
+
+    def __sub__(self, value):
+        self.value-=value
+        self.normalize()
+        return self
+
+    def __str__(self):
+        return "Angle({})".format(self.value)
+
+    def add(self, value):
+        return self.__add__(value)
+
+
+class Vector(object):
+    @staticmethod
+    def from_pt(x1,y1,x2,y2):
+        deltaY = y2 - y1
+        deltaX = x2 - x1
+        angle=math.atan2(deltaY, deltaX)
+        value=math.sqrt(deltaX**2+deltaY**2)
+        return Vector(angle, value)
+
+    def __init__(self, angle,value):
+        self.angle=Angle(angle)
+        self.value=value
+
+    def apply(self,x,y):
+        vx,vy=apply_rot(self.angle.value,self.value,0.0)
+        return x+vx,y+vy
+
+    def __add__(self, vector):
+        x1,y1=self.apply(0,0)
+        x2,y2=vector.apply(0,0)
+
+        xn,yn=x1+x2,y1+y2
+
+        value = math.sqrt(xn**2+yn**2)
+
+        angle = math.atan2(yn,xn)
+
+        return Vector(angle, value)
+
+    def __str__(self):
+        x1,y1=self.apply(0,0)
+        return "angle={} value={} x={} y={}".format(self.angle, self.value, x1, y1)
+
 
 class Asset:
 
@@ -74,6 +161,7 @@ class Player(Asset):
 class Ball(Asset):
     def __init__(self, game,p):
         self.bs = Gbs
+        self.previous_data = None
         Asset.__init__(self, game, 240, 290, 260, 310)
         self.v=5
         #direction de depart aleatoire
@@ -83,14 +171,20 @@ class Ball(Asset):
             self.dx = self.bs
         self.dy=0
 
+        self.vector=Vector.from_pt(0, 0, self.dx, self.dy)
+
     def __str__(self):
         return "x1:{self.x1};x2:{self.x2};y1:{self.y1};y2:{self.y2}"
 
     def draw(self):
-        self.x2 += self.dx
-        self.y2 += self.dy
-        self.y1 += self.dy
-        self.x1 += self.dx
+        #self.x2 += self.dx
+        #self.y2 += self.dy
+        #self.y1 += self.dy
+        #self.x1 += self.dx
+
+        self.x1, self.y1 = self.vector.apply(self.x1, self.y1)
+        self.x2, self.y2 = self.vector.apply(self.x2, self.y2)
+
         #test si on sort pas du cadre
         if self.x1 <= 5:
             self.x1 = 6
@@ -114,9 +208,12 @@ class Ball(Asset):
                 #centre de la balle
                 yb = self.y1+10
                 #calcul trajectoire sur y en prenant la distance par rapport au centre de la raquette
-                self.dy = (yb-p1.y1-50)*0.2
+                # self.dy = (yb-p1.y1-50)*0.2
                 #inversion du sens de la balle + augmentation vitesse
-                self.dx = -self.dx
+                # self.dx = -self.dx
+
+                self.vector.angle.value = -self.vector.angle.value
+
                 #calcul fictif du parcours de la balle
                 self.fictif_route()
             else:
@@ -134,9 +231,12 @@ class Ball(Asset):
                 #centre de la balle
                 yb = self.y1+10
                 #calcul trajectoire sur y en prenant la distance par rapport au centre de la raquette
-                self.dy = (yb-p2.y1-50)*0.2
+                # self.dy = (yb-p2.y1-50)*0.2
                 #inversion du sens de la balle + augmentation vitesse
-                self.dx = -self.dx
+                # self.dx = -self.dx
+
+                self.vector.angle.value = -self.vector.angle.value
+
                 #calcul fictif du parcours de la balle
                 self.fictif_route()
             else:
@@ -180,5 +280,7 @@ class Ball(Asset):
     
     def sendScore(self, p1, p2):
         data = {'left':p1.score,'right':p2.score}
-        headers = {'Content-Type':'application/json'}
-        future = session.post(server + '/api/score', data=json.dumps(data), headers=headers)
+        if data != self.previous_data:
+	        headers = {'Content-Type':'application/json'}
+	        future = session.post(server + '/api/score', data=json.dumps(data), headers=headers)
+	        self.previous_data = data
